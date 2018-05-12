7bcd420b69a52f10114ea593a464bdf35074b632
Ovidio R. Calvet <calveto@gmail.com>
2018-05-11 22:32:27 -0400

    add multiple commands

--- src/App.js
+++ src/App.js
@@ -2,19 +2,22 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  async takeOff() {
+  async perform(action) {
     try {
-      const response = await fetch("http://localhost:9081/tello/takeoff");
+      const response = await fetch(`http://localhost:9081/tello/${action}`);
       const json = await response.json();
       console.log('JSON', json);
     } catch(e) {
-      console.log('E', e);
+      console.log(`error performing ${action}`, e);
     }
   }
   render() {
     return (
       <div>
-        <button onClick={() => {this.takeOff()}}>Take Off</button>
+        <button onClick={() => {this.perform('takeoff')}}>Take Off</button>
+        <button onClick={() => {this.perform('land')}}>Land</button>
+        <button onClick={() => {this.perform('forward')}}>Forward</button>
+        <button onClick={() => {this.perform('back')}}>Back</button>
       </div>
     );
   }
